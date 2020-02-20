f42c20d2b7a10c880c59b151b243e63bfcd6497a
Jens <jensthebens@gmail.com>
2020-02-20 20:38:07 +0000

    sh

---
+++ gitpull.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Pulling latest changes from github .."
+
+gitpull=$(git pull 2>&1)
+echo "$gitpull"
+
+if [ gitpull | grep "Fast-Forward" -n]; then
+
+    check_config=$(hass --script check_config -i -c /config/ | grep "Successful config (all)" 2>&1)
+    echo "$check_config"
+
+    if [ check_config == "Successful config (all)" ]; then
+        echo "Config check successfull !"
+        restart=$(reboot now 2>&1)
+    fi
+
+fi
+
+# https://github.com/Tommatheussen/Home-Assistant-Configuration/tree/master/to-migrate/bin
